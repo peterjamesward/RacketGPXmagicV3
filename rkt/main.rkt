7e6a841f0274c45e39b117fcad8aa04476fbb444
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-12-20 17:35:14 +0000

    Change label.

--- rkt/main.rkt
+++ rkt/main.rkt
@@ -1,18 +1,18 @@
 #lang racket/gui
 
 ; Make a frame by instantiating the frame% class
-(define frame (new frame% [label "Example"]))
+(define frame (new frame% [label "GPXmagic v3"]))
  
 ; Make a static text message in the frame
 (define msg (new message% [parent frame]
-                          [label "No events so far..."]))
+                          [label "No track loaded."]))
  
 ; Make a button in the frame
 (new button% [parent frame]
-             [label "Click Me"]
+             [label "Load a GPX file"]
              ; Callback procedure for a button click:
              [callback (lambda (button event)
-                         (send msg set-label "Button click"))])
+                         (send msg set-label "Not yet implemented."))])
  
 ; Show the frame by calling its show method
 (send frame show #t)
