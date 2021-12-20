32b278099b50f581a296e6345a349a95e5302e3c
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-12-20 18:26:32 +0000

    Button displays file name.

--- rkt/main.rkt
+++ rkt/main.rkt
@@ -7,12 +7,19 @@
 (define msg (new message% [parent frame]
                           [label "No track loaded."]))
  
-; Make a button in the frame
+(define (read-gpx-file button event)
+  (define gpx-file-path (get-file "Read GPX file" frame #f #f "gpx" '()
+                      '(("GPX Files" "*.gpx") ("Any" "*.*"))))
+  (define gpx-file-name 
+    (some-system-path->string
+      (file-name-from-path gpx-file-path)))
+  (send msg set-label gpx-file-name))
+  
+  ; Make a button in the frame
 (new button% [parent frame]
              [label "Load a GPX file"]
              ; Callback procedure for a button click:
-             [callback (lambda (button event) (get-file "Read GPX file" frame #f #f "gpx" '()
-                                  '(("GPX Files" "*.gpx") ("Any" "*.*"))))])
+             [callback read-gpx-file])
  
 ; Show the frame by calling its show method
 (send frame show #t)
