c4558c610c95aa6decc16682b18d0a0d0c71416a
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-12-21 17:43:54 +0000

    Track on a map

--- rkt/import-from-gpx.rkt
+++ rkt/import-from-gpx.rkt
@@ -4,20 +4,25 @@
 
 (require "track.rkt"
          xml
+         sugar
          xml/path)
 
-(define (trackpoint-from-gpx trkpt)
-  (let ([longitude (string->number (se-path* '(trkpt #:lon) trkpt))]
-        [latitude (string->number (se-path* '(trkpt #:lat) trkpt))]
-        [altitude (string->number (se-path* '(trkpt ele) trkpt))])
-    (print altitude)
-    (trackpoint longitude latitude altitude)))
+
+(define (trackpoint-helper longitudes latitudes altitudes)
+  (if (or (empty? longitudes) (empty? latitudes) (empty? altitudes))
+      null 
+      (cons (trackpoint (string->number (first longitudes))
+                        (string->number (first latitudes))
+                        (string->number (first altitudes)))
+            (trackpoint-helper (rest longitudes) (rest latitudes) (rest altitudes)))))
 
 
 (define (import-from-gpx file-path)
   (define file-name (some-system-path->string (file-name-from-path file-path)))
   (define as-xml (xml->xexpr (document-element (read-xml (open-input-file file-path)))))
   (define track-name (se-path* '(gpx trk name) as-xml))
-  (define raw-points (se-path*/list '(gpx trk trkseg) as-xml))
-  (define trackpoints (map trackpoint-from-gpx raw-points))
+  (define longitudes (se-path*/list '(gpx trk trkseg trkpt #:lon) as-xml))
+  (define latitudes (se-path*/list '(gpx trk trkseg trkpt #:lat) as-xml))
+  (define altitudes (se-path*/list '(gpx trk trkseg trkpt ele) as-xml))
+  (define trackpoints (trackpoint-helper longitudes latitudes altitudes))
   (track file-name track-name trackpoints null))
