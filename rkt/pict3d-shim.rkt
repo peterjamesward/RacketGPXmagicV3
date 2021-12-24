64db45e364f04809d03cfb23f248251abb8377bf
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-12-24 10:42:24 +0000

    Pan & Zoom.

--- rkt/pict3d-shim.rkt
+++ rkt/pict3d-shim.rkt
@@ -15,7 +15,7 @@
            [camera-distance 1000] ; metres away from whatever the focal point is (default to origin)
            [camera-azimuth 180] ; where the camera is relative to the origin
            [camera-elevation 30] ; from XY plane
-           [dragging #f]
+           [mouse-start #f]
            [zoom 12])
 
     (define/public (camera-from-azimuth-elevation)
@@ -26,18 +26,47 @@
         (set! my-camera new-camera)
         (send this set-pict3d (combine my-pict my-camera))))
 
-    (define/override (on-event mouse-event)
-      (let ([parent (send this get-parent)]) (send parent set-label "MOUSEY")))
-    ; Pan & rotate here.
-    ;      (lambda (mouse-event) #f))
+    (define origin-x 0)
+    (define origin-y 0)
+    (define last-mouse-x #f)
+    (define last-mouse-y #f)
+
+    ;; Handle a mouse event.  Return #t if the event was handled, #f
+    ;; otherwise.
+    (define/override (on-event event)
+      (cond
+        [(send event button-down? 'left)
+         (set! last-mouse-x (send event get-x))
+         (set! last-mouse-y (send event get-y))
+         ;; Return as "Not handled', let others maybe handle it
+         #f]
+        [(send event button-up? 'left)
+         (set! last-mouse-x #f)
+         (set! last-mouse-y #f)
+         ;; Return as "Not handled', let others maybe handle it
+         #f]
+        [(send event dragging?)
+         (let ([mouse-x (send event get-x)] [mouse-y (send event get-y)])
+           (when (and last-mouse-x last-mouse-y)
+             (set! origin-x (- origin-x (- mouse-x last-mouse-x)))
+             (set! origin-y (- origin-y (- last-mouse-y mouse-y)))
+             (set! camera-azimuth origin-x)
+             (set! camera-elevation origin-y)
+             (send this camera-from-azimuth-elevation)
+             (set! last-mouse-x mouse-x)
+             (set! last-mouse-y mouse-y)))
+         ;; Event was handled
+         #t]
+        ;; Not handled
+        [#t #f]))
+
 
     (define/override (on-char event)
       ; Because mouse wheel comes in like this.
       (let ([parent (send this get-parent)] [which-key (send event get-key-code)])
         (cond
-          [(equal? which-key 'wheel-up) (set! camera-distance (min 1000 (* camera-distance 1.1)))]
-          [(equal? which-key 'wheel-down) (set! camera-distance (max 2 (* camera-distance 0.9)))]
-          [#t (send parent set-label "other")])
+          [(equal? which-key 'wheel-down) (set! camera-distance (min 1000 (* camera-distance 1.1)))]
+          [(equal? which-key 'wheel-up) (set! camera-distance (max 2 (* camera-distance 0.9)))])
         (send this camera-from-azimuth-elevation)
         #t))
 
